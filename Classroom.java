5b8c2b697d7ffb29aaf42138339e5b1d39ed8dd9
Austin Okada <2470539@lfschools.net>
2021-01-13 19:58:13 +0000

    for now

--- Classroom.java
+++ Classroom.java
@@ -1,1 +1,28 @@
+public class Classroom{
+  private String name;
+  private String students;
+  private int num_students;
+  private final int MAX_STUDENTS = 30;
+  public Classroom(String theName){
+    name = theName;
+  }
+  public String getName(){
+    return name;
+  }
+  public String setName(String aName){
+    name = aName;
+    return name;
+  }
+  public int getNumStudents(){
+  return num_students;  
+  }
+  public String addStudents(String student){
+    students = students + " " + student;
+    return students.trim();
 
+  }
+  public String toString(){
+    return students.trim();
+  }
+
+}
--- Sample.java
+++ Sample.java
@@ -1,1 +1,12 @@
 
+public class Sample{
+  public Sample(){
+
+  }
+  public void create(){}
+  Classroom classes = new Classroom("Aronson");
+  classes.addStudents("Sam");
+  classes.addStudents("Sara");
+  classes.addStudents("Nate");
+
+}
